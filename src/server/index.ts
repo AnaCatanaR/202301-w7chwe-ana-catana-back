0a86e54a95d08e9317a62fba23ad1d0695ee1783
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-25 17:07:33 +0100

    add sonar propertys and github workflow to project

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,8 @@
 import morgan from "morgan";
 import express from "express";
-import cors from "cors";
 import usersRouter from "./routers/usersRouter.js";
 
 const app = express();
-
-app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json());
